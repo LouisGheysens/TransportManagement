8fb151bdd5ea0ca968a00dca8cde3e2c1ffd54c8
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-01-18 22:44:44 +0100

    WPF step 2: add default windows

---
+++ WPF/ChauffeurWindows/ChauffeurWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WPF.ChauffeurWindows
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChauffeurWindow : ContentPage
+    {
+        public ChauffeurWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPF/OngevalWindows/OngevalWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WPF.OngevalWindows
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OngevalWindow : ContentPage
+    {
+        public OngevalWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
